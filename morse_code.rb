07b63750f69add69bc1088f2c32cc851754abaa4
yasinabdmahmood <abd181103@gmail.com>
2022-11-02 18:27:33 +0300

    Refactor get_word methed

--- morse_code.rb
+++ morse_code.rb
@@ -7,17 +7,18 @@ def get_char(char)
   }
   morse_dict[char]
 end
-
+puts get_char('.-') #=> A
 def get_word(arr)
   word = ''
-  arr.each { |i| word += get_char(i) }
+  arr.split.each { |i| word += get_char(i) }
   word
 end
+puts get_word("-- -.--") #=> MY
 
 def decode(str)
   sentese = ''
   arry = str.split('   ')
-  arry.each { |i| sentese += "#{get_word(i.split)} " }
+  arry.each { |i| sentese += "#{get_word(i)} " }
   sentese
 end
 x = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
