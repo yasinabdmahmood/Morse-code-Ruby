b4bc864c7efb48e5a2f5c69a7de5e570afb1f5eb
Mirwais Farahi <mirfarahi92@gmail.com>
2022-11-04 08:03:04 +0500

    Edit morse_code.rb File

--- morse_code.rb
+++ morse_code.rb
@@ -21,5 +21,14 @@ def decode(str)
   arry.each { |i| sentese += "#{get_word(i)} " }
   sentese
 end
+
+def decode(string)
+  a = ''
+  string.split('  ').each do |alpha|
+    a += decode_word(alpha) + ' '
+  end
+  a
+end
+
 x = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 puts x
